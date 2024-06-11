778a00c470b28d361fc85d53b82e662481513e78
Diksha19997 <kdmdikshaghosh555@gmail.com>
2024-06-12 01:06:55 +0530

    committing first project-1

---
+++ src/main/java/com/example/demo/controller/FirstProjectController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Service.FirstService;
+
+@RestController
+@RequestMapping("/FirstProject")
+public class FirstProjectController {
+	
+	@Autowired
+	private FirstService service;
+	
+	@GetMapping("/hello")
+	public String helloPage() {
+		return service.s1();
+	}
+
+}
